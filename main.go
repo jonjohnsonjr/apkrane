e39bfb259f2351e29f84655e7c3fe63eec90dbfe
Jon Johnson <jon.johnson@chainguard.dev>
2023-08-30 14:48:42 -0700

    Support -

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -40,11 +39,49 @@ func ls() *cobra.Command {
 		Args:    cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			u := args[0]
-			if scheme, _, ok := strings.Cut(u, "://"); ok && strings.HasPrefix(scheme, "http") {
-				return httpIndex(cmd.OutOrStdout(), u, full, j)
+
+			var in io.Reader
+
+			dir := ""
+
+			if u == "-" {
+				in = cmd.InOrStdin()
+			} else if scheme, _, ok := strings.Cut(u, "://"); ok && strings.HasPrefix(scheme, "http") {
+				rc, err := fetchIndex(u)
+				if err != nil {
+					return err
+				}
+				defer rc.Close()
+
+				in = rc
+				dir = strings.TrimSuffix(u, "/APKINDEX.tar.gz")
+			} else {
+				return fmt.Errorf("todo: handle filepaths")
 			}
 
-			return fmt.Errorf("todo: handle filepaths")
+			index, err := repository.IndexFromArchive(io.NopCloser(in))
+			if err != nil {
+				return fmt.Errorf("parsing %q: %w", u, err)
+			}
+
+			w := cmd.OutOrStdout()
+			enc := json.NewEncoder(w)
+
+			for _, pkg := range index.Packages {
+				p := fmt.Sprintf("%s-%s.apk", pkg.Name, pkg.Version)
+				u := fmt.Sprintf("%s/%s", dir, p)
+				if j {
+					if err := enc.Encode(pkg); err != nil {
+						return fmt.Errorf("encoding %s: %w", pkg.Name, err)
+					}
+				} else if full {
+					fmt.Fprintf(w, "%s\n", u)
+				} else {
+					fmt.Fprintf(w, "%s\n", p)
+				}
+			}
+
+			return nil
 		},
 	}
 
@@ -54,41 +91,14 @@ func ls() *cobra.Command {
 	return cmd
 }
 
-func httpIndex(w io.Writer, u string, full bool, j bool) error {
+func fetchIndex(u string) (io.ReadCloser, error) {
 	resp, err := http.Get(u)
 	if err != nil {
-		return fmt.Errorf("GET %q: %w", u, err)
+		return nil, fmt.Errorf("GET %q: %w", u, err)
 	}
-	defer resp.Body.Close()
-
 	if resp.StatusCode >= 400 {
-		return fmt.Errorf("GET %q: status %d", u, resp.StatusCode)
+		return nil, fmt.Errorf("GET %q: status %d", u, resp.StatusCode)
 	}
 
-	index, err := repository.IndexFromArchive(io.NopCloser(bufio.NewReaderSize(resp.Body, 1<<20)))
-	if err != nil {
-		return fmt.Errorf("parsing %q: %w", u, err)
-	}
-
-	dir := strings.TrimSuffix(u, "/APKINDEX.tar.gz")
-
-	var enc *json.Encoder
-	if j {
-		enc = json.NewEncoder(w)
-	}
-	for _, pkg := range index.Packages {
-		p := fmt.Sprintf("%s-%s.apk", pkg.Name, pkg.Version)
-		u := fmt.Sprintf("%s/%s", dir, p)
-		if j {
-			if err := enc.Encode(pkg); err != nil {
-				return fmt.Errorf("encoding %s: %w", pkg.Name, err)
-			}
-		} else if full {
-			fmt.Fprintf(w, "%s\n", u)
-		} else {
-			fmt.Fprintf(w, "%s\n", p)
-		}
-	}
-
-	return nil
+	return resp.Body, nil
 }
